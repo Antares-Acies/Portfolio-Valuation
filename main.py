bbba55ac8f2cdefe47647184118fa2415dcf666c
Kumar Akashdeep <kumarakashdeep@acies.holdings>
2025-02-03 00:31:14 +0530

    start of intiate process

---
+++ main.py
@@ -0,0 +1,16 @@
+#trigger platform reponsible for tiggering portfolio valaution's 
+
+
+import pandas as pd
+import numpy as np 
+
+from Portfolio_Valuation import final_valuation_fn
+
+def main():
+    final_valuation_fn(config_dict, request, data=None)
+    return 0
+
+
+
+if __name__ == "__main__":
+    main()
